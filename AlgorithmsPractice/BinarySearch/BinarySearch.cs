b85bdcdf7b15c987aa7de99748931d15c4150f4c
Ifeanyi Shadrach Odom <ifeanyishadrach452@gmail.com>
2024-01-08 11:37:46 +0100

    Commit all

--- AlgorithmsPractice/BinarySearch/BinarySearch.cs
+++ AlgorithmsPractice/BinarySearch/BinarySearch.cs
@@ -34,7 +34,26 @@ public class BinarySearch
         return a;
     }
 
+    public Tree GenerateIntBinaryTree()
+    {
+        Tree a = new Tree("3");
+        Tree b = new Tree("11");
+        Tree c = new Tree("4");
+        Tree d = new Tree("4");
+        Tree e = new Tree("2");
+        Tree f = new Tree("1");
+
+        a.Left = b;
+        a.Right = c;
+        b.Left = d;
+        b.Right = e;
+        c.Right = f;
+
+        return a;
+    }
+
     #region Depth First Traversal
+
     public void DepthFirstValues(Tree? binaryTree)
     {
         if (binaryTree == null)
@@ -69,7 +88,7 @@ public class BinarySearch
     }
 
     #endregion
-    
+
     #region Breadth First Traversal
 
     public void BreadthFirstValues(Tree? binaryTree)
@@ -84,15 +103,255 @@ public class BinarySearch
         {
             var currentNode = queueNodes.Dequeue();
             Console.WriteLine("Current node is " + currentNode.Value);
-            
+
             //Check if right value is not null, then enqueue
-            if(currentNode.Right != null)
+            if (currentNode.Right != null)
                 queueNodes.Enqueue(currentNode.Right);
             //Check if left value is not null, then enqueue
-            if(currentNode.Left != null)
+            if (currentNode.Left != null)
                 queueNodes.Enqueue(currentNode.Left);
-            
         }
+    }
+
+    #endregion
+
+    #region CheckIfValueExistsDepthFirstTraversal
+
+    public bool DoesTargetExists(Tree? tree, string targetValue)
+    {
+        if (tree == null) return false;
+
+        Stack<Tree?> stackNodes = new();
+        stackNodes.Push(tree);
+
+        while (stackNodes.Count > 0)
+        {
+            var currentNode = stackNodes.Pop();
+            if (currentNode?.Value.ToString() == targetValue)
+                return true;
+
+            if (currentNode?.Left != null)
+                stackNodes.Push(currentNode.Left);
+
+            if (currentNode?.Right != null)
+                stackNodes.Push(currentNode.Right);
+        }
+
+        return false;
+    }
+
+    public bool DoesTargetExistsRecursive(Tree? tree, string targetValue)
+    {
+        if (tree == null) return false;
+        if (tree.Value.ToString() == targetValue) return true;
+        //Traverse Left                                         //Traverse Right
+        return DoesTargetExistsRecursive(tree.Left, targetValue) || DoesTargetExistsRecursive(tree.Right, targetValue);
+    }
+
+    #endregion
+
+    #region CheckIfValueExistsBreadthFirstTraversal
+
+    public bool DoesTargetExistsB(Tree? tree, string targetValue)
+    {
+        if (tree == null) return false;
+
+        Queue<Tree?> queueNodes = new();
+        queueNodes.Enqueue(tree);
+
+        while (queueNodes.Count > 0)
+        {
+            var currentNode = queueNodes.Dequeue();
+            if (currentNode?.Value.ToString() == targetValue)
+                return true;
+
+            if (currentNode?.Left != null)
+                queueNodes.Enqueue(currentNode.Left);
+
+            if (currentNode?.Right != null)
+                queueNodes.Enqueue(currentNode.Right);
+        }
+
+        return false;
+    }
+
+    #endregion
+
+    #region SumValuesDepthFirst
+
+    public int SumTreeNodes(Tree? nodes)
+    {
+        if (nodes == null) return 0;
+        Stack<Tree> stackNodes = new();
+        stackNodes.Push(nodes);
+        int count = 0;
+        while (stackNodes.Count > 0)
+        {
+            var currentNode = stackNodes.Pop();
+            count += Convert.ToInt32(currentNode.Value.ToString());
+
+            if (currentNode.Left != null)
+                stackNodes.Push(currentNode.Left);
+            if (currentNode.Right != null)
+                stackNodes.Push(currentNode.Right);
+        }
+
+        return count;
+    }
+
+    public int SumTreeNodesRecursively(Tree? nodes)
+    {
+        if (nodes == null) return 0;
+
+        return Convert.ToInt32(nodes.Value.ToString()) + SumTreeNodesRecursively(nodes.Left) +
+               SumTreeNodesRecursively(nodes.Right);
+    }
+
+    #endregion
+
+    #region SumValuesBreadthFirst
+
+    public int SumTreeNodesB(Tree? nodes)
+    {
+        if (nodes == null) return 0;
+        Queue<Tree> queueNodes = new();
+        queueNodes.Enqueue(nodes);
+        int count = 0;
+        while (queueNodes.Count > 0)
+        {
+            var currentNode = queueNodes.Dequeue();
+            count += Convert.ToInt32(currentNode.Value.ToString());
+
+            if (currentNode.Left != null)
+                queueNodes.Enqueue(currentNode.Left);
+            if (currentNode.Right != null)
+                queueNodes.Enqueue(currentNode.Right);
+        }
+
+        return count;
+    }
+
+    #endregion
+
+    #region MinimumValuesDepththFirst
+
+    public int GetMinimumValuesDepthFirst(Tree? nodes)
+    {
+        if (nodes == null)
+            return 0;
+        Stack<Tree> stackNodes = new Stack<Tree>();
+        stackNodes.Push(nodes);
+
+        int minimumValue = int.MaxValue;
+        while (stackNodes.Count > 0)
+        {
+            var currentNode = stackNodes.Pop();
+            minimumValue = Math.Min(minimumValue, Convert.ToInt32(currentNode.Value.ToString()));
+
+            if (currentNode.Left != null)
+                stackNodes.Push(currentNode.Left);
+
+            if (currentNode.Right != null)
+                stackNodes.Push(currentNode.Right);
+        }
+
+        return minimumValue;
+    }
+    public int GetMinimumValuesDepthFirstRecursive(Tree? nodes)
+    {
+        if (nodes == null)
+            return int.MaxValue;
+        
+        int leftMin = GetMinimumValuesDepthFirstRecursive(nodes.Left);
+        int rightMin = GetMinimumValuesDepthFirstRecursive(nodes.Right);
+
+        return Math.Min(Math.Min(leftMin, rightMin), Convert.ToInt32(nodes.Value.ToString()));
+
+    }
+    public int GetMinimumValuesRecursively(Tree? nodes, int minimumValue)
+    {
+        if (nodes == null)
+            return int.MaxValue;
+         // if (Convert.ToInt32(nodes.Value.ToString()) < minimumValue)
+         //     return Convert.ToInt32(nodes.Value.ToString());
+
+        //Traverse Left                                                         //Traverse Right
+        return Math.Min(
+            Math.Min(GetMinimumValuesRecursively(nodes.Left, minimumValue),
+                GetMinimumValuesRecursively(nodes.Right, minimumValue)), Convert.ToInt32(nodes.Value.ToString()));
+    }
+
+    #endregion
+
+    #region MinimumValuesBreadthFirst
+
+    public int GetMinimumValuesBreadthFirst(Tree? nodes)
+    {
+        if (nodes == null)
+            return 0;
+        Queue<Tree> queueNodes = new Queue<Tree>();
+        queueNodes.Enqueue(nodes);
+
+        int minimumValue = int.MaxValue;
+        while (queueNodes.Count > 0)
+        {
+            var currentNode = queueNodes.Dequeue();
+            minimumValue = Math.Min(minimumValue, Convert.ToInt32(currentNode.Value.ToString()));
+
+            if (currentNode.Left != null)
+                queueNodes.Enqueue(currentNode.Left);
+
+            if (currentNode.Right != null)
+                queueNodes.Enqueue(currentNode.Right);
+        }
+
+        return minimumValue;
+    }
+
+    #endregion
+    
+    
+    #region GetMaximumSumPath
+
+    public int GetMaximumSumPath(Tree nodes)
+    {
+        if (nodes == null) 
+            return 0;
+        
+        int maxLeftPath = 0, maxRightPath = 0;
+        maxLeftPath += Convert.ToInt32(nodes.Value.ToString()) + GetMaximumSumPath(nodes.Left);
+        maxRightPath += Convert.ToInt32(nodes.Value.ToString()) + GetMaximumSumPath(nodes.Right);
+
+        return Math.Max(maxLeftPath, maxRightPath);
+    }
+    
+    public int MinOperations(int[] nums) {
+        if(nums.Length == 0) return -1;
+
+        Dictionary<int, int> addNumbers = new Dictionary<int, int>();
+        foreach(var num in nums){
+            if(!addNumbers.ContainsKey(num))
+                addNumbers.Add(num, 1);
+            else
+                addNumbers[num] += 1;
+        }
+
+        int count = 0;
+        foreach (var item in addNumbers)
+        {
+            if (item.Value == 1)
+                return -1;
+            count += item.Value / 3;
+            if (item.Value % 2 == 0)
+                count += 1;
+        }
+        return count;
+    }
+    
+    public string AddBinary(string a, string b)
+    {
+        int value = Convert.ToInt32(b) % Convert.ToInt32(a);
+        return value.ToString();
     }
     #endregion
 }
--- AlgorithmsPractice/Program.cs
+++ AlgorithmsPractice/Program.cs
@@ -6,14 +6,61 @@
         {
             BinarySearch.BinarySearch binarySearch = new BinarySearch.BinarySearch();
             var binaryTree = binarySearch.GenerateBinaryTree();
+            var binaryIntTree = binarySearch.GenerateIntBinaryTree();
             
-            binarySearch.DepthFirstValues(binaryTree);
-            Console.WriteLine("--------------------------------");
-            binarySearch.DepthFirstValuesRecursively(binaryTree);
+            // binarySearch.DepthFirstValues(binaryTree);
+            // Console.WriteLine("--------------------------------");
+            // binarySearch.DepthFirstValuesRecursively(binaryTree);
+            //
+            // Console.WriteLine("--------------------------------");
+            // Console.WriteLine("--------------------------------");
+            // binarySearch.BreadthFirstValues(binaryTree);
+            //
+            // Console.WriteLine("--------------------------------");
+            // Console.WriteLine("Checking if target Exists using Depth First Search");
+            // Console.WriteLine("Target found = " + binarySearch.DoesTargetExists(binaryTree, "e"));
+            // Console.WriteLine("Checking if target Exists using Depth First Search Recursively");
+            // Console.WriteLine("Target found = " + binarySearch.DoesTargetExistsRecursive(binaryTree, "e"));
+            // Console.WriteLine("--------------------------------");
+            // Console.WriteLine("Checking if target Exists using Breadth First Search");
+            // Console.WriteLine("Target found = " + binarySearch.DoesTargetExistsB(binaryTree, "e"));
             
+            // Console.WriteLine("--------------------------------");
+            // Console.WriteLine("Depth First Sum");
+            // Console.WriteLine("Sum of integers is = " + binarySearch.SumTreeNodes(binaryIntTree));
+            //
+            // Console.WriteLine("--------------------------------");
+            // Console.WriteLine("Depth First Sum Recursively");
+            // Console.WriteLine("Sum of integers is = " + binarySearch.SumTreeNodesRecursively(binaryIntTree));
+            //
+            //
+            // Console.WriteLine("--------------------------------");
+            // Console.WriteLine("Breadth First Sum");
+            // Console.WriteLine("Sum of integers is = " + binarySearch.SumTreeNodesB(binaryIntTree));
+            //
+            // Console.WriteLine("--------------------------------");
+            // Console.WriteLine("Depth First Minimum Value");
+            // Console.WriteLine("Minimum value in the tree is = " + binarySearch.GetMinimumValuesDepthFirst(binaryIntTree));
+            //
+            // Console.WriteLine("--------------------------------");
+            // Console.WriteLine("Depth First Minimum Value Recursively");
+            // Console.WriteLine("Minimum value in the tree is = " + binarySearch.GetMinimumValuesDepthFirstRecursive(binaryIntTree));
+            // Console.WriteLine("Minimum value in the tree is = " + binarySearch.GetMinimumValuesRecursively(binaryIntTree, int.MaxValue));
+            //
+            //
+            // Console.WriteLine("--------------------------------");
+            // Console.WriteLine("Breadth First Minimum Value");
+            // Console.WriteLine("Minimum value in the tree is = " + binarySearch.GetMinimumValuesBreadthFirst(binaryIntTree));
+            //
+            //             
             Console.WriteLine("--------------------------------");
-            Console.WriteLine("--------------------------------");
-            binarySearch.BreadthFirstValues(binaryTree);
+            // Console.WriteLine("Maximum path value in the tree is = " + binarySearch.GetMaximumSumPath(binaryIntTree));
+            // int[] numberArray = new[] { 2, 3, 3, 2, 2, 4, 2, 3, 4 };
+            //
+            // var dictionaryObtained = binarySearch.MinOperations(numberArray);
+            
+            Console.WriteLine("Binary sum is = " + binarySearch.AddBinary("11", "1"));
+            
             // See https://aka.ms/new-console-template for more information
             //Console.WriteLine("Hello, World!");
         }
